cbdfacbb7ad133bd1f0de3f7dba51b68b09fdf8d
Steve Wang <wangs@SNHD.ORG>
2025-08-21 22:58:07 -0700

    folder added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'LLM AI Project',
+    version= '0.0.0',
+    author= 'SteveWang',
+    author_email= 'steve.wang@nau.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
